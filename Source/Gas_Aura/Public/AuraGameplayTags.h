3ebeb947587b443cb178e5e451955e1016b3fd41
qingmusang <370941566@qq.com>
2026-02-11 23:16:04 +0800

    feat: Update GameplayTagsManager

---
+++ Source/Gas_Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,27 @@
+// Copyright Daisy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+/**
+ *AuraGameplayTags
+ *
+ * Singleton containing native gameplay tags
+ */
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get(){return GameplayTags;}
+	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
+	
+protected:
+
+private:
+	static FAuraGameplayTags GameplayTags;
+
+
+	
+};
