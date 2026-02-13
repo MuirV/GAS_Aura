de9cefd30989f84fc70e26ff0871eb62d5c8d454
qingmusang <370941566@qq.com>
2026-02-13 14:35:22 +0800

    NewFile: AttributeInfo(Heritied from DataAsset)

---
+++ Source/Gas_Aura/Public/AbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,17 @@
+// Copyright Daisy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
