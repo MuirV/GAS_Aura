3f7608bcab16c55afc78ea5cb3b076a0f818e627
qingmusang <370941566@qq.com>
2026-02-13 17:40:16 +0800

    chore: AttributeMenuWidgetController(inherited from AuraWidgetController

---
+++ Source/Gas_Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Daisy
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
---
+++ Source/Gas_Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,20 @@
+// Copyright Daisy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BindCallbacksToDependencies() override;
+	virtual void BroadcastInitialValues() override;
+	
+};
