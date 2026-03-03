ae7664e89b9cc99de9367354b6ff1a11442fa56f
qingmusang <370941566@qq.com>
2026-03-03 22:51:21 +0800

    feat: Set by Caller

--- Source/Gas_Aura/Private/AuraGameplayTags.cpp
+++ Source/Gas_Aura/Private/AuraGameplayTags.cpp
@@ -109,4 +109,9 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
     FName("InputTag.4"),
     FString("Input Tag for 4 Button")
     );
+
+    GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+   FName("Damage"),
+   FString("Damage")
+   );
 }
