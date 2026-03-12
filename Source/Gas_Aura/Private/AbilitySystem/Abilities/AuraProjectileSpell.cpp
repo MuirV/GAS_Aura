74e724e703f5611dfd088744a45c795c8b7ebdca
qingmusang <370941566@qq.com>
2026-03-12 21:13:04 +0800

    feat: FGameplayEffectContext, 新建AuraAbilityTypes继承

--- Source/Gas_Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Gas_Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -46,7 +46,18 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 
 		//待办：给予Projectile ，GE造成Damage
 		const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
-		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
+		FGameplayEffectContextHandle EffectContextHandle = SourceASC->MakeEffectContext();
+		EffectContextHandle.SetAbility(this);
+		EffectContextHandle.AddSourceObject(Projectile);
+		TArray<TWeakObjectPtr<AActor>> Actors;
+		Actors.Add(Projectile);
+		EffectContextHandle.AddActors(Actors);
+		FHitResult HitResult;
+		HitResult.Location = ProjectileTargetLocation;
+		EffectContextHandle.AddHitResult(HitResult);
+
+		
+		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), EffectContextHandle);
 
 		FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
 		const float ScaledDamage = Damage.GetValueAtLevel(5);
@@ -57,6 +68,7 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 		
 		
 		Projectile->FinishSpawning(SpawnTransform);
+		
 	}
 		
 	
