08fb95ee6d6818dd62e23779c90dadd433cf6f1f
qingmusang <370941566@qq.com>
2026-03-15 22:36:54 +0800

    feat: BTService. 新增两个变量BB（黑板键）FBlackboardKeySelector

--- Source/Gas_Aura/Public/AI/BTService_FindNearestPlayer.h
+++ Source/Gas_Aura/Public/AI/BTService_FindNearestPlayer.h
@@ -15,4 +15,10 @@ class GAS_AURA_API UBTService_FindNearestPlayer : public UBTService_BlueprintBas
 	GENERATED_BODY()
 protected:
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector TargetToFollowSelector;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FBlackboardKeySelector DistanceToFollowSelector;
 };
