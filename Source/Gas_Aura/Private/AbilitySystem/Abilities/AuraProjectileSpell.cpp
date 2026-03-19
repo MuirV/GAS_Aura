27bff59ed5377d01385429024561463ba85cf640
qingmusang <370941566@qq.com>
2026-03-19 09:33:45 +0800

    feat: Weapon Socket Tags (delete former Melee Tag)

--- Source/Gas_Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Gas_Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -27,7 +27,10 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
 	if (CombatInterface)
 	{
-		const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo());
+		const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(
+			GetAvatarActorFromActorInfo(),
+			FAuraGameplayTags::Get().Montage_Attack_Weapon
+			);
 		//FVector SocketLocation = CombatInterface->GetCombatSocketLocation();  optimized
 		FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
 		//Rotation.Pitch = 0.f; //保持水平旋转 Client模式下相比较客户端会导致火球高度不一致 
