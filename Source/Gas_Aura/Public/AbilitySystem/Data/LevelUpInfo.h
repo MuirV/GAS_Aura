815b040bc10cf264f8693383d656a24c98cf0438
qingmusang <370941566@qq.com>
2026-03-25 15:25:11 +0800

    fix

---
+++ Source/Gas_Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,17 @@
+// Copyright Daisy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
